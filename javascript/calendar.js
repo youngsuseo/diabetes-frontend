bb84e059df9e3a7d2976aff1ad71026438ae2428
youngsuseo <56705264+youngsuseo@users.noreply.github.com>
2023-04-20 00:15:46 +0900

    feat 화면 layout 및 기능 변경

--- javascript/calendar.js
+++ javascript/calendar.js
@@ -1,65 +1,11 @@
 document.addEventListener('DOMContentLoaded', function () {
     const calendarEl = document.getElementById('calendar');
     const calendar = new FullCalendar.Calendar(calendarEl, {
-        initialView: 'dayGridMonth',
-        locale: 'ko',
-        navLinks: true, // 날짜를 선택하면 Day 캘린더나 Week 캘린더로 링크
-        editable: true, // 수정 가능?
-        selectable: true, // 달력 일자 드래그 설정가능
-        nowIndicator: true, // 현재 시간 마크
-        dayMaxEvents: true, // 이벤트가 오버되면 높이 제한 (+ 몇 개식으로 표현)
-        select: function(arg) { // 캘린더에서 드래그로 이벤트를 생성할 수 있다.
-            var title = prompt('Event Title:');
-            if (title) {
-                calendar.addEvent({
-                    title: title,
-                    start: arg.start,
-                    end: arg.end,
-                    allDay: arg.allDay
-                })
-            }
-            calendar.unselect()
+        initialView: 'timeGridWeek',
+        headerToolbar: {
+            left: 'prev,next',
+            right: 'timeGridWeek,timeGridDay' // user can switch between the two
         },
-        events: [
-            {
-                title: '이벤트 1',
-                start: '2023-05-01'
-            },
-            {
-                title: '이벤트 2',
-                start: '2023-05-05',
-                end: '2023-05-07'
-            },
-            {
-                title: '이벤트 3',
-                start: '2023-05-09T16:00:00'
-            }
-        ]
     });
     calendar.render();
-
-    document.getElementById('addEvent').addEventListener('click', function () {
-
-        fetch('http://127.0.0.1:3000/meals', {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json',
-            },
-            // body: JSON.stringify(userInfoWithQuery)
-        })
-            .then((response) => response.json())
-            .then((data) => {
-                const event = {
-                    title: data.title,
-                    start: data.start,
-                    allDay: true
-                };
-
-                // 캘린더에 이벤트를 추가합니다.
-                calendar.addEvent(event);
-            })
-            .catch((error) => {
-                alert("식단을 조회하는 과정에서 에러가 발생하였습니다.");
-            });
-    });
 });
--- javascript/chat.js
+++ javascript/chat.js
@@ -13,29 +13,21 @@ document.addEventListener('DOMContentLoaded', () => {
         const userInput = document.getElementById('user-input');
         const message = document.createElement('div');
 
-        let userInfo = sessionStorage.getItem("userInfo");
-
-        let parse = JSON.parse(userInfo);
-        const userInfoWithQuery = {
-            gender: parse.gender,
-            age: parse.age,
-            weight: parse.weight,
-            bloodSugar: parse.bloodSugar,
-            userQuery: userInput.value
-        };
-
         message.textContent = `User: ${userInput.value}`;
         chatMessages.appendChild(message);
+
+        console.log(userInput.value);
 
         fetch('http://127.0.0.1:3000/foods', {
             method: 'POST',
             headers: {
                 'Content-Type': 'application/json',
             },
-            body: JSON.stringify(userInfoWithQuery)
+            body: JSON.stringify({"userQuery": userInput.value}),
         })
             .then((response) => response.json())
             .then((data) => {
+
                 const successMessage = document.createElement('div');
                 successMessage.className = 'success-message';
                 // I want to show the error message right side of the chat
@@ -55,49 +47,5 @@ document.addEventListener('DOMContentLoaded', () => {
             });
 
         userInput.value = '';
-    });
-
-    infoSubmitButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        saveToStorage();
-    });
-
-    function saveToStorage() {
-        const gender = document.getElementById("gender").value;
-        const age = document.getElementById("age").value;
-        const weight = document.getElementById("weight").value;
-        const bloodSugar = document.getElementById("blood-sugar").value;
-
-        // 사용자 정보를 객체로 생성
-        const userInfo = {
-            gender: gender,
-            age: parseInt(age),
-            weight: parseFloat(weight),
-            bloodSugar: parseFloat(bloodSugar)
-        };
-
-        // 객체를 JSON 문자열로 변환
-        const userInfoJSON = JSON.stringify(userInfo);
-
-        // Session Storage에 JSON 문자열을 저장
-        sessionStorage.setItem("userInfo", userInfoJSON);
-
-        chatSection.hidden = false;
-        userInfoForm.parentElement.hidden = true;
-        calenderSection.hidden = true;
-    }
-
-    // 캘린더 버튼 클릭시 캘린더 보여주기
-    calendarButton.addEventListener('click', (e) => {
-        e.preventDefault();
-        calenderSection.hidden = false;
-        chatSection.hidden = true;
-        userInfoForm.parentElement.hidden = true;
-    });
-
-    // 식단 버튼 클릭시 식단 보여주기
-    makeMealsButton.addEventListener('click', (e) => {
-        e.preventDefault();
-
     });
 });
